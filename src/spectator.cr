1a734106a5cc86c582ccbca008a02a5645f1f0d2
drhuffman12 <drhuffman12@gmail.com>
2020-11-29 21:24:12 -0500

    drhuffman for now, disable 'scripts/test_junit_format'

--- src/spectator.cr
+++ src/spectator.cr
@@ -7,6 +7,7 @@ module Spectator
 
   # Current version of the Spectator library.
   VERSION = "0.9.28"
+  # VERSION = "0.9.29"
 
   # Top-level describe method.
   # All specs in a file must be wrapped in this call.
