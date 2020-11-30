5f5bd170999a5b2ec73b04332c819d5fb007a1e7
drhuffman12 <drhuffman12@gmail.com>
2020-11-29 21:34:46 -0500

    drhuffman12 add some comments re 'random_string' methods

--- spec/docs/helper_methods_spec.cr
+++ spec/docs/helper_methods_spec.cr
@@ -2,6 +2,7 @@ Spectator.describe String do
   include StringHelpers
 
   describe "#size" do
+    # random_string uses method with param
     subject { random_string(10).size }
 
     it "is the length of the string" do
@@ -14,6 +15,7 @@ Spectator.describe String do
   include StringHelpers
 
   describe "#size" do
+    # random_string uses method with NO param (uses 'let(length)' instead)
     let(length) { 10 } # random_string uses this.
     subject { random_string.size }
 
