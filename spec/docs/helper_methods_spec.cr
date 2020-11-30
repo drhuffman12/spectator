7be48a4d0f35383f7b4966b615c202e603afbc90
drhuffman12 <drhuffman12@gmail.com>
2020-11-29 21:32:10 -0500

    drhuffman12 adjust helper methods

--- spec/docs/helper_methods_spec.cr
+++ spec/docs/helper_methods_spec.cr
@@ -1,11 +1,5 @@
 Spectator.describe String do
-  # This is a helper method.
-  def random_string(length)
-    chars = ('a'..'z').to_a
-    String.build(length) do |builder|
-      length.times { builder << chars.sample }
-    end
-  end
+  include StringHelpers
 
   describe "#size" do
     subject { random_string(10).size }
@@ -15,39 +9,12 @@ Spectator.describe String do
     end
   end
 end
-Spectator.describe String do
-  # length is now pulled from value defined by `let`.
-  def random_string
-    chars = ('a'..'z').to_a
-    String.build(length) do |builder|
-      length.times { builder << chars.sample }
-    end
-  end
-
-  describe "#size" do
-    let(length) { 10 } # random_string uses this.
-    subject { random_string.size }
-
-    it "is the length of the string" do
-      is_expected.to eq(length)
-    end
-  end
-end
-
-module StringHelpers
-  def random_string
-    chars = ('a'..'z').to_a
-    String.build(length) do |builder|
-      length.times { builder << chars.sample }
-    end
-  end
-end
 
 Spectator.describe String do
   include StringHelpers
 
   describe "#size" do
-    let(length) { 10 }
+    let(length) { 10 } # random_string uses this.
     subject { random_string.size }
 
     it "is the length of the string" do
--- spec/spec_helper.cr
+++ spec/spec_helper.cr
@@ -11,3 +11,21 @@ end
 macro specify_fails(description = nil, &block)
   it_fails {{description}} {{block}}
 end
+
+module StringHelpers
+  # This is a helper method.
+  def random_string(length)
+    chars = ('a'..'z').to_a
+    String.build(length) do |builder|
+      length.times { builder << chars.sample }
+    end
+  end
+
+  # length is now pulled from value defined by `let`.
+  def random_string
+    chars = ('a'..'z').to_a
+    String.build(length) do |builder|
+      length.times { builder << chars.sample }
+    end
+  end
+end
